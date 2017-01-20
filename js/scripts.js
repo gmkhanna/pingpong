0e1253d3587949447e21308be9e4d18eae8bcd4d
Grinil Khanna <gmkhanna@gmail.com>
2017-01-20 12:29:20 -0800

    Separate working factorial section with attempting integer list section

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,44 @@
 
 
-
-var oneToNum = function(integer) {
-  if ((integer % 3 === 0) && (integer % 5 === 0)) {
-  return "ping-pong";
-  } else if (integer % 5 === 0) {
-  return "pong";
-  } else if (integer % 3 === 0) {
-  return "ping";
-  } else {
-    return integer;
-  }
-};
+integer = 0;
 
 
+function convertList() {
+  var integerArray = []
 
+for (var i = 1; i <= integer; i++) {
+  integerArray.push(i);
+}
 
+console.log(integerArray);
+}
 
+// for (var i = 0; i <= 5001; i++) {
+//   integerList.push(i);
+// }
+// console.log(integerList[i]);
+
+// var integerDiv = function(integerDiv) {
+//   if ((integerDiv % 3 === 0) && (integerDiv % 5 === 0)) {
+//   return "ping-pong";
+// } else if (integerDiv % 5 === 0) {
+//   return "pong";
+// } else if (integerDiv % 3 === 0) {
+//   return "ping";
+// } else if ((integerDiv < 0) || (integerDiv > 5001)) {
+//     return "Come on now... Don't you think I gave you a good range to play with?";
+//   } else {
+//     return integerDiv
+//   }
+// };
 
 $(document).ready(function() {
   $("form#pingForm").submit(function(event) {
     event.preventDefault();
     var integer = parseInt($("input#number").val());
-    var result = oneToNum(integer);
+    // var result = integerDiv(integer);
+
+    var result = integerArray(integer);
     $("#result").text(result);
 
   });
@@ -36,3 +52,12 @@ $(document).ready(function() {
 // simplest may be counting up to integer - or from integer down to 1
 
 // You will be using "%"  i.e. 3 % === 0 && 5 % === 0; //
+
+// function factor(n) {
+//   factor = 0
+//
+//   for (i = 1; i <= n; i++) {
+//     n = n + i;
+//   }
+//   return factor;
+// }
